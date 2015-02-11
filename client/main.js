fcf1ef9f53a7b008d8ec592ecdb2645da14aa53d
Spinaldash <blueavenger@gmail.com>
2015-02-11 09:18:52 -0800

    keypress implementation Please enter the commit message for your changes. Lines starting

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@
 $(document).ready(init);
 var notCheating = '0';
 var operator = '0';
+var x = 0;
 function init() {
   $('.round').click(changeDisplay);
   $('#clear').click(clearDisplay);
@@ -9,15 +10,42 @@ function init() {
   $('.symbol').click(operatorAssignment);
   $('#rootOp').click(root);
 }
-
 $(document).keypress(function(e) {
   if (e.shiftKey && e.keyCode === 43) { // run on +
-    var x = String.fromCharCode(e.keyCode);
+    x = String.fromCharCode(e.keyCode);
     keyOperatorAssignment(x);
-    }
-
+  }
+  if (e.keyCode >= 48 && e.keyCode <= 57) { // run on 0-9
+    x = String.fromCharCode(e.keyCode);
+    keyChangeDisplay(x);
+  }
+  if (e.shiftKey && e.keyCode === 42) { // run on *
+    x = String.fromCharCode(e.keyCode);
+    keyOperatorAssignment(x);
+  }
+  if (e.keyCode === 45) { // run on -
+    x = String.fromCharCode(e.keyCode);
+    keyOperatorAssignment(x);
+  }
+  if (e.keyCode === 47) { // run on /
+    x = String.fromCharCode(e.keyCode);
+    keyOperatorAssignment(x);
+  }
+  if (e.keyCode === 61) { // run on =
+    equals(x);
+  }
+  if (e.shiftKey && e.keyCode === 94) { // run on ^
+    x = String.fromCharCode(e.keyCode);
+    keyOperatorAssignment(x);
+  }
+  if (e.shiftKey && e.keyCode === 67) { // run on C
+    clearDisplay();
+  }
+  if (e.keyCode === 46) { // run on .
+    x = String.fromCharCode(e.keyCode);
+    keyChangeDisplay(x);
+  }
 });
-
 function changeDisplay() { // Runs on circle button press
   var oldText = $('#display').text();
   if (this.textContent === '.') { //<= Special case for .
@@ -57,8 +85,24 @@ function operatorAssignment() {
   $('#display').text('0');
 }
 function keyOperatorAssignment(keyDownOperator) {
-  console.log('keyDownOperator');
   operator = keyDownOperator;
   notCheating = $('#display').text();
   $('#display').text('0');
+}
+function keyChangeDisplay(keyDownButton) {
+    // Runs on round keypress
+    var oldText = $('#display').text();
+    var input = keyDownButton;
+    if (input === '.') { //<= Special case for .
+      if ($('#display').text().indexOf('.') === -1) {
+        $('#display').text(oldText + input);
+      } return;
+    }
+    if (input === '-') { // Special +- case
+      $('#display').text(oldText * -1);
+    } else if ($('#display').text() === '0') { //
+      $('#display').text(input);
+    } else {
+      $('#display').text(oldText + input);
+    }
 }
